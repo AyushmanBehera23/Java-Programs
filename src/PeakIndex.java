75db8a57109c9f346841a43b6f1cdbf0aef687b9
ayushmanbehera23 <ayushman0426@gmail.com>
2026-06-30 12:26:22 +0530

    Java

---
+++ src/PeakIndex.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class PeakIndex {
+    static int MountainArray(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] < arr[mid + 1]) {
+                start = mid + 1;
+            } else {
+                end = mid;
+            }
+        }
+        return start;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int peak = MountainArray(arr);
+
+        System.out.println("Peak Index: " + peak);
+        System.out.println("Peak Element: " + arr[peak]);
+    }
+}
