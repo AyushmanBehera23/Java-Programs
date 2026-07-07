a4bcb2659a2d9b19317b79c16c8b955f2451a7f7
ayushmanbehera23 <ayushman0426@gmail.com>
2026-07-07 10:58:30 +0530

    Java

--- src/MiddleNode.java
+++ src/MiddleNode.java
@@ -3,14 +3,9 @@ public class MiddleNode {
     static class ListNode {
         int val;
         ListNode next;
-        ListNode() {
-        }
+
         ListNode(int val) {
             this.val = val;
-        }
-        ListNode(int val, ListNode next) {
-            this.val = val;
-            this.next = next;
         }
     }
 
---
+++ src/ReverseLinkedList.java
@@ -0,0 +1,40 @@
+public class ReverseLinkedList {
+    static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+    static ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        while (head != null) {
+            ListNode temp = head.next;
+            head.next = prev;
+            prev = head;
+            head = temp;
+        }
+        return prev;
+    }
+    static void display(ListNode head) {
+        ListNode temp = head;
+        while (temp != null) {
+            System.out.print(temp.val + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("NULL");
+    }
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        System.out.println("Original Linked List:");
+        display(head);
+        head = reverseList(head);
+        System.out.println("Reversed Linked List:");
+        display(head);
+    }
+}
