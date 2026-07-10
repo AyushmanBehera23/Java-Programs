d70b10c5d701faa8e0068ee96b160a2ff60fe28e
ayushmanbehera23 <ayushman0426@gmail.com>
2026-07-10 22:27:51 +0530

    Java

---
+++ src/NearestSmallerElement.java
@@ -0,0 +1,59 @@
+import java.util.*;
+
+public class NearestSmallerElement{
+
+    public static ArrayList<Integer> Smallestonleft(ArrayList<Integer> arr, int n) {
+
+        Stack<Integer> stack = new Stack<>();
+        ArrayList<Integer> ans = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+
+            if (stack.isEmpty()) {
+                ans.add(-1);
+            }
+
+            else if (stack.peek() < arr.get(i)) {
+                ans.add(stack.peek());
+            }
+
+            else {
+
+                while (!stack.isEmpty() && stack.peek() >= arr.get(i)) {
+                    stack.pop();
+                }
+
+                if (stack.isEmpty()) {
+                    ans.add(-1);
+                } else {
+                    ans.add(stack.peek());
+                }
+            }
+
+            stack.push(arr.get(i));
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        ArrayList<Integer> arr = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            arr.add(sc.nextInt());
+        }
+
+        ArrayList<Integer> ans = Smallestonleft(arr, n);
+
+        for (int x : ans) {
+            System.out.print(x + " ");
+        }
+
+        sc.close();
+    }
+}
---
+++ src/StockSpan.java
@@ -0,0 +1,51 @@
+import java.util.*;
+public class StockSpan {
+
+    public class StockSpan {
+
+        static ArrayList<Integer> calculateSpan(int[] arr) {
+
+            Stack<int[]> st = new Stack<>();
+            ArrayList<Integer> res = new ArrayList<>();
+
+            for (int i = 0; i < arr.length; i++) {
+
+                while (!st.isEmpty() && st.peek()[0] <= arr[i]) {
+                    st.pop();
+                }
+
+                if (st.isEmpty()) {
+                    res.add(i + 1);
+                } else {
+                    res.add(i - st.peek()[1]);
+                }
+
+                st.push(new int[]{arr[i], i});
+            }
+
+            return res;
+        }
+
+        public static void main(String[] args) {
+
+            Scanner sc = new Scanner(System.in);
+
+            int n = sc.nextInt();
+
+            int[] arr = new int[n];
+
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+
+            ArrayList<Integer> ans = calculateSpan(arr);
+
+            for (int x : ans) {
+                System.out.print(x + " ");
+            }
+
+            sc.close();
+        }
+    }
+
+}
