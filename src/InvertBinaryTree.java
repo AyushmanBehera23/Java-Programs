cb390f70d36d539d764bf106e5ffac923ff7e375
ayushmanbehera23 <ayushman0426@gmail.com>
2026-07-17 10:30:07 +0530

    Java

---
+++ src/InvertBinaryTree.java
@@ -0,0 +1,65 @@
+public class InvertBinaryTree {
+
+    // Tree Node
+    static class TreeNode {
+        int val;
+        TreeNode left, right;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    // Invert Binary Tree
+    static TreeNode invertTree(TreeNode root) {
+
+        if (root == null) {
+            return null;
+        }
+
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+
+        root.left = right;
+        root.right = left;
+
+        return root;
+    }
+
+    // Inorder Traversal
+    static void inorder(TreeNode root) {
+
+        if (root == null) {
+            return;
+        }
+
+        inorder(root.left);
+        System.out.print(root.val + " ");
+        inorder(root.right);
+    }
+
+    public static void main(String[] args) {
+
+        // Creating Tree
+        TreeNode root = new TreeNode(4);
+
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(7);
+
+        root.left.left = new TreeNode(1);
+        root.left.right = new TreeNode(3);
+
+        root.right.left = new TreeNode(6);
+        root.right.right = new TreeNode(9);
+
+        System.out.print("Original Tree (Inorder): ");
+        inorder(root);
+
+        invertTree(root);
+
+        System.out.println();
+
+        System.out.print("Inverted Tree (Inorder): ");
+        inorder(root);
+    }
+}
