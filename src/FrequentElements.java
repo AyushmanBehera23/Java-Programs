24e94a0343f43d859a5ae1a440174062b9b898be
ayushmanbehera23 <ayushman0426@gmail.com>
2026-07-20 14:31:21 +0530

    Java

---
+++ src/FrequentElements.java
@@ -0,0 +1,32 @@
+
+import java.util.*;
+class Solution {
+    public class FrequentElements {
+        public int[] topKFrequent(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        // Count frequency
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        // Max Heap based on frequency
+        PriorityQueue<Integer> pq = new PriorityQueue<>(
+                (a, b) -> map.get(b) - map.get(a)
+        );
+
+        // Add all keys into heap
+        for (int key : map.keySet()) {
+            pq.offer(key);
+        }
+
+        // Take top k elements
+        int[] ans = new int[k];
+
+        for (int i = 0; i < k; i++) {
+            ans[i] = pq.poll();
+        }
+
+        return ans;
+    }
+}
